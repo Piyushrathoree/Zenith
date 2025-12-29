646a77c083533976a41ebf88449751ce8b3ac9b9
Piyushrathoree <meonpath008@gmail.com>
2025-12-29 16:18:02 +0530

    added some dependencies

--- apps/services/planner-service/src/index.ts
+++ apps/services/planner-service/src/index.ts
@@ -5,11 +5,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 const PORT = process.env.PORT!
 
-
 const startServer = () => {
 
     connectDB()
-
     app.listen(PORT, () => {
         console.log(`planner service is running at port ${PORT}`)
     })
