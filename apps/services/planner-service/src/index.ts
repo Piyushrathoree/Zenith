2e0a70a081e08ad763c4f7fe1d0d09d3ccba05d4
Piyushrathoree <meonpath008@gmail.com>
2026-01-04 01:27:31 +0530

    Revert "added some dependencies"
    This reverts commit 646a77c083533976a41ebf88449751ce8b3ac9b9.

--- apps/services/planner-service/src/index.ts
+++ apps/services/planner-service/src/index.ts
@@ -5,9 +5,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 const PORT = process.env.PORT!
 
+
 const startServer = () => {
 
     connectDB()
+
     app.listen(PORT, () => {
         console.log(`planner service is running at port ${PORT}`)
     })
