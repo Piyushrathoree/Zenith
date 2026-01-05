bf75d44f551e225ebae3027f49f5753d6e457041
Piyushrathoree <meonpath008@gmail.com>
2026-01-05 21:56:15 +0530

    feat: Add initial root and dashboard pages.

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { DashboardLayout } from "@/components/DashboardLayout";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+export default function Page() {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <DashboardLayout />
+        <Toaster />
+        <Sonner />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,22 +1,11 @@
-"use client";
+'use client';
 
-import { DashboardLayout } from "@/components/DashboardLayout";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import React from 'react'
 
-export default function Page() {
-  const [queryClient] = useState(() => new QueryClient());
-
+const page = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider >
-        <DashboardLayout />
-        <Toaster />
-        <Sonner />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+    <div>hello world</div>
+  )
 }
+
+export default page
