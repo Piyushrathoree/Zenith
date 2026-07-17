d90b68e0a22b0e3d363c9976888aa3346921b0eb
PIYUSH RATHORE <163632958+Piyushrathoree@users.noreply.github.com>
2026-07-17 22:58:35 +0530

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Instrument_Serif, Alan_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 
